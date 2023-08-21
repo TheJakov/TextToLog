de43b9dc40301c152c6b0f32953c798a5a2787d5
TheJakov <kristovic.jakov@gmail.com>
2023-08-21 19:10:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿if (args.Length != 1) return;
+
+string filePath = args[0];
+
+if (filePath == null) return;
+if (!File.Exists(filePath)) return;
+
+byte[] bytes = File.ReadAllBytes(filePath);
+
+string directoryPatch = Path.GetDirectoryName(filePath);
+string fileName = Path.GetFileName(filePath);
+string newPath = Path.Join(directoryPatch, fileName.Split('.').First()) + ".log";
+
+File.WriteAllBytes(newPath, bytes);
